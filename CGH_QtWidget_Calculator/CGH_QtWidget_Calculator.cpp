3d846a4e177947e0bee831556ad7b11e6b3278a6
ghchoi21 <gh.choi@protnc.co.kr>
2021-11-19 17:57:15 +0900

    [Modify] Qt Calculator Program update :: implement  operation of addtion, subtraction, Multiplication, division

--- CGH_QtWidget_Calculator/CGH_QtWidget_Calculator.cpp
+++ CGH_QtWidget_Calculator/CGH_QtWidget_Calculator.cpp
@@ -1,7 +1,182 @@
 #include "CGH_QtWidget_Calculator.h"
+#include "calculator.hpp"
+#include <string>
+#include <QMessageBox>
+#include <iostream>
 
-CGH_QtWidget_Calculator::CGH_QtWidget_Calculator(QWidget *parent)
+
+CGH_QtWidget_Calculator::CGH_QtWidget_Calculator(QWidget* parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    connect(ui.pushButton_0, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_0);
+    connect(ui.pushButton_1, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_1);
+    connect(ui.pushButton_2, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_2);
+    connect(ui.pushButton_3, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_3);
+    connect(ui.pushButton_4, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_4);
+    connect(ui.pushButton_5, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_5);
+    connect(ui.pushButton_6, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_6);
+    connect(ui.pushButton_7, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_7);
+    connect(ui.pushButton_8, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_8);
+    connect(ui.pushButton_9, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_9);
+
+    connect(ui.pushButton_plus, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_plus);
+    connect(ui.pushButton_minus, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_minus);
+    connect(ui.pushButton_multiple, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_multiple);
+    connect(ui.pushButton_divide, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_divide);
+
+    connect(ui.pushButton_result, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_result);
+    connect(ui.pushButton_clearExpr, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_clearExpr);
+    connect(ui.pushButton_resetCalc, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_resetCalc);
+    connect(ui.pushButton_backspace, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_backspace);
+    connect(ui.pushButton_selectNegOrPos, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_selectNegOrPos);
+
+
+}
+
+void CGH_QtWidget_Calculator::on_Button_selectNegOrPos()
+{
+    auto strPrev = ui.lineEdit_num->text();
+
+    if (strPrev != NULL)
+    {
+        auto stdPrev = strPrev.toStdString();
+        auto checkMinus = stdPrev.substr(0, 1);
+        if (checkMinus != "-")
+        {
+            auto strModi = "-" + stdPrev;
+            ui.lineEdit_num->setText(QString::fromStdString(strModi));
+        }
+        else
+        {
+            auto strModi = stdPrev.substr(1, stdPrev.length() - 1);
+            ui.lineEdit_num->setText(QString::fromStdString(strModi));
+        }
+    }
+    else
+    {
+        QMessageBox::warning(this, "Error Expression!", "There is no exist data of expression. Input the Expression!");
+    }
+
+}
+void CGH_QtWidget_Calculator::on_Button_backspace()
+{
+    auto strPrev = ui.lineEdit_num->text();
+    auto stdPrev = strPrev.toStdString();
+    auto strModi = stdPrev.substr(0, stdPrev.length() - 1);
+    ui.lineEdit_num->setText(QString::fromStdString(strModi));
+}
+
+void CGH_QtWidget_Calculator::on_Button_clearExpr()
+{
+    ui.lineEdit_num->setText("");
+}
+
+void CGH_QtWidget_Calculator::on_Button_resetCalc()
+{
+    ui.lineEdit_num->setText("");
+    ui.lineEdit_expr->setText("");
+}
+
+//void CGH_QtWidget_Calculator::setCalcData(QString str)          //구현중
+//{
+//    auto strPrev = ui.lineEdit_expr->text();
+//    int strNum = std::stoi(str.toStdString());
+//    QString strLine_num;
+//    QString strLine_expr;
+//
+//    if (0 <= strNum && strNum <= 9)
+//    {
+//        strLine_num = strPrev + str;
+//        ui.lineEdit_num->setText(strLine_num);
+//    }
+//
+//    strLine_expr = strPrev + str;
+//
+//    ui.lineEdit_expr->setText(strLine_expr);
+//}
+
+void CGH_QtWidget_Calculator::setCalcData(QString str)          //구현중
+{
+    auto strPrev = ui.lineEdit_num->text();
+  
+    strPrev += str;
+
+    ui.lineEdit_num->setText(strPrev);
+    ui.lineEdit_expr->setText(strPrev);
+}
+
+
+void CGH_QtWidget_Calculator::on_Button_result()
+{
+    auto strExpr = ui.lineEdit_num->text();
+    if (strExpr != NULL)
+    {
+        auto stdExpr = strExpr.toStdString();
+        auto nResult = calculator::eval(stdExpr);
+        ui.lineEdit_num->setText(QString::number(nResult));
+    }
+    else
+    {
+        QMessageBox::warning(this, "Error Expression!", "There is no exist data of expression. Input the Expression!");
+    }
+    
+}
+void CGH_QtWidget_Calculator::on_Button_0()
+{
+    setCalcData("0");
+}
+void CGH_QtWidget_Calculator::on_Button_1()
+{
+    setCalcData("1");
+}
+void CGH_QtWidget_Calculator::on_Button_2()
+{
+    setCalcData("2");
+}
+void CGH_QtWidget_Calculator::on_Button_3()
+{
+    setCalcData("3");
+}
+void CGH_QtWidget_Calculator::on_Button_4()
+{
+    setCalcData("4");
+}
+void CGH_QtWidget_Calculator::on_Button_5()
+{
+    setCalcData("5");
+}
+void CGH_QtWidget_Calculator::on_Button_6()
+{
+    setCalcData("6");
+}
+void CGH_QtWidget_Calculator::on_Button_7()
+{
+    setCalcData("7");
+}
+void CGH_QtWidget_Calculator::on_Button_8()
+{
+    setCalcData("8");
+}
+void CGH_QtWidget_Calculator::on_Button_9()
+{
+    setCalcData("9");
+}
+
+void CGH_QtWidget_Calculator::on_Button_plus()
+{
+    setCalcData("+");
+}
+void CGH_QtWidget_Calculator::on_Button_minus()
+{
+    setCalcData("-");
+}
+void CGH_QtWidget_Calculator::on_Button_multiple()
+{
+    setCalcData("*");
+}
+void CGH_QtWidget_Calculator::on_Button_divide()
+{
+    setCalcData("/");
 }
--- CGH_QtWidget_Calculator/CGH_QtWidget_Calculator.h
+++ CGH_QtWidget_Calculator/CGH_QtWidget_Calculator.h
@@ -8,8 +8,35 @@ class CGH_QtWidget_Calculator : public QMainWindow
     Q_OBJECT
 
 public:
-    CGH_QtWidget_Calculator(QWidget *parent = Q_NULLPTR);
+    CGH_QtWidget_Calculator(QWidget* parent = Q_NULLPTR);
 
 private:
     Ui::CGH_QtWidget_CalculatorClass ui;
+
+public:
+    //사용자 지정 함수
+    void setCalcData(QString str);  //lineEdit 위젯에 버튼으로 입력한 데이터를 문자열로 누적 출력함
+
+    // 버튼 UI 슬롯 : 버튼의 문자를 lineEdit 위젯으로 전달
+    void on_Button_0();
+    void on_Button_1();
+    void on_Button_2();
+    void on_Button_3();
+    void on_Button_4();
+    void on_Button_5();
+    void on_Button_6();
+    void on_Button_7();
+    void on_Button_8();
+    void on_Button_9();
+
+    void on_Button_plus();
+    void on_Button_minus();
+    void on_Button_multiple();
+    void on_Button_divide();
+
+    void on_Button_result();
+    void on_Button_clearExpr();
+    void on_Button_resetCalc();
+    void on_Button_backspace();
+    void on_Button_selectNegOrPos();
 };
