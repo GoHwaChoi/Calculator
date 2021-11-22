c48fe7614ab5172e62a5f72db2e97dc79f93532d
ghchoi21 <gh.choi@protnc.co.kr>
2021-11-22 14:29:36 +0900

    [bugFix] MainWindow 창 크기 조절 시 내부 UI 크기 자동 조절되도록 Qt파라미터 설정

--- CGH_QtWidget_Calculator/CGH_QtWidget_Calculator.cpp
+++ CGH_QtWidget_Calculator/CGH_QtWidget_Calculator.cpp
@@ -10,6 +10,7 @@ CGH_QtWidget_Calculator::CGH_QtWidget_Calculator(QWidget* parent)
 {
     ui.setupUi(this);
 
+    //숫자버튼 입력
     connect(ui.pushButton_0, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_0);
     connect(ui.pushButton_1, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_1);
     connect(ui.pushButton_2, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_2);
@@ -21,11 +22,13 @@ CGH_QtWidget_Calculator::CGH_QtWidget_Calculator(QWidget* parent)
     connect(ui.pushButton_8, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_8);
     connect(ui.pushButton_9, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_9);
 
+    //연산자 버튼 입력
     connect(ui.pushButton_plus, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_plus);
     connect(ui.pushButton_minus, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_minus);
     connect(ui.pushButton_multiple, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_multiple);
     connect(ui.pushButton_divide, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_divide);
 
+    //부가기능 버튼 입력
     connect(ui.pushButton_result, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_result);
     connect(ui.pushButton_clearExpr, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_clearExpr);
     connect(ui.pushButton_resetCalc, &QAbstractButton::clicked, this, &CGH_QtWidget_Calculator::on_Button_resetCalc);
@@ -35,17 +38,17 @@ CGH_QtWidget_Calculator::CGH_QtWidget_Calculator(QWidget* parent)
 
 }
 
-void CGH_QtWidget_Calculator::on_Button_selectNegOrPos()
+void CGH_QtWidget_Calculator::on_Button_selectNegOrPos()        //양수, 음수 전환
 {
-    auto strPrev = ui.lineEdit_num->text();
+    auto strPrev = ui.lineEdit_num->text();     //숫자 입력창에서 텍스트 가져옴
 
-    if (strPrev != NULL)
+    if (! strPrev.isEmpty())                    //입력창의 텍스트가 있는 경우
     {
-        auto stdPrev = strPrev.toStdString();
-        auto checkMinus = stdPrev.substr(0, 1);
-        if (checkMinus != "-")
+        auto stdPrev = strPrev.toStdString();   //QString을 String 타입으로 변환
+        auto checkMinus = stdPrev.substr(0, 1); //문자열 중 첫번째 문자 1개만 추출
+        if (checkMinus != "-")                  //숫자 입력 시 - 부호를 입력한 경우
         {
-            auto strModi = "-" + stdPrev;
+            auto strModi = "-" + stdPrev;       //
             ui.lineEdit_num->setText(QString::fromStdString(strModi));
         }
         else
@@ -54,13 +57,13 @@ void CGH_QtWidget_Calculator::on_Button_selectNegOrPos()
             ui.lineEdit_num->setText(QString::fromStdString(strModi));
         }
     }
-    else
+    else                                      //입력창의 텍스트가 없는 경우 예외 처리
     {
         QMessageBox::warning(this, "Error Expression!", "There is no exist data of expression. Input the Expression!");
     }
 
 }
-void CGH_QtWidget_Calculator::on_Button_backspace()
+void CGH_QtWidget_Calculator::on_Button_backspace()     //마지막 텍스트 지우기(backspace)
 {
     auto strPrev = ui.lineEdit_num->text();
     auto stdPrev = strPrev.toStdString();
@@ -68,36 +71,37 @@ void CGH_QtWidget_Calculator::on_Button_backspace()
     ui.lineEdit_num->setText(QString::fromStdString(strModi));
 }
 
-void CGH_QtWidget_Calculator::on_Button_clearExpr()
+void CGH_QtWidget_Calculator::on_Button_clearExpr()     //입력된 피연산자 문자열 삭제
 {
     ui.lineEdit_num->setText("");
 }
 
-void CGH_QtWidget_Calculator::on_Button_resetCalc()
+void CGH_QtWidget_Calculator::on_Button_resetCalc()     //입력된 계산식 및 피연산자 문자열 삭제
 {
     ui.lineEdit_num->setText("");
     ui.lineEdit_expr->setText("");
 }
 
-//void CGH_QtWidget_Calculator::setCalcData(QString str)          //구현중
-//{
-//    auto strPrev = ui.lineEdit_expr->text();
-//    int strNum = std::stoi(str.toStdString());
-//    QString strLine_num;
-//    QString strLine_expr;
-//
-//    if (0 <= strNum && strNum <= 9)
-//    {
-//        strLine_num = strPrev + str;
-//        ui.lineEdit_num->setText(strLine_num);
-//    }
-//
-//    strLine_expr = strPrev + str;
-//
-//    ui.lineEdit_expr->setText(strLine_expr);
-//}
+void CGH_QtWidget_Calculator::setCalcData_followingWinApp(QString str)          //구현중 : 연산자 입력 시 상단 계산식에만 추가. 하단은 피연산자만 입력
+{
+    //return;
+    auto strPrev = ui.lineEdit_expr->text();
+    int strNum = std::stoi(str.toStdString());
+    QString strLine_num;
+    QString strLine_expr;
 
-void CGH_QtWidget_Calculator::setCalcData(QString str)          //구현중
+    if (0 <= strNum && strNum <= 9)
+    {
+        strLine_num = strPrev + str;
+        ui.lineEdit_num->setText(strLine_num);
+    }
+
+    strLine_expr = strPrev + str;
+
+    ui.lineEdit_expr->setText(strLine_expr);
+}
+
+void CGH_QtWidget_Calculator::setCalcData(QString str)          //버튼으로 입력된 내용 출력(계산식)
 {
     auto strPrev = ui.lineEdit_num->text();
   
@@ -108,7 +112,7 @@ void CGH_QtWidget_Calculator::setCalcData(QString str)          //구현중
 }
 
 
-void CGH_QtWidget_Calculator::on_Button_result()
+void CGH_QtWidget_Calculator::on_Button_result()                //계산식 결과값 연산 버튼 슬롯
 {
     auto strExpr = ui.lineEdit_num->text();
     if (strExpr != NULL)
@@ -123,7 +127,9 @@ void CGH_QtWidget_Calculator::on_Button_result()
     }
     
 }
-void CGH_QtWidget_Calculator::on_Button_0()
+
+//숫자버튼 슬롯
+void CGH_QtWidget_Calculator::on_Button_0()     
 {
     setCalcData("0");
 }
@@ -164,6 +170,7 @@ void CGH_QtWidget_Calculator::on_Button_9()
     setCalcData("9");
 }
 
+//연산자 버튼 슬롯
 void CGH_QtWidget_Calculator::on_Button_plus()
 {
     setCalcData("+");
--- CGH_QtWidget_Calculator/CGH_QtWidget_Calculator.h
+++ CGH_QtWidget_Calculator/CGH_QtWidget_Calculator.h
@@ -15,7 +15,8 @@ private:
 
 public:
     //사용자 지정 함수
-    void setCalcData(QString str);  //lineEdit 위젯에 버튼으로 입력한 데이터를 문자열로 누적 출력함
+    void setCalcData(QString str);                  //lineEdit_num 위젯에 버튼으로 입력한 데이터를 문자열로 누적 출력함
+    void setCalcData_followingWinApp(QString str);  //lineEdit_num 위젯(하단)에서는 숫자만, lineEdit_expr 위젯(상단)에는 계산식 표현. Win계산기처럼
 
     // 버튼 UI 슬롯 : 버튼의 문자를 lineEdit 위젯으로 전달
     void on_Button_0();
